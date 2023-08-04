de3ca62cf75c17668467c391733cf7f70e80ede3
omarinux <81483478+omarinux@users.noreply.github.com>
2023-08-04 13:50:10 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 # Define Terraform provider
 terraform {
-  required_version = ">= 1.3"
   backend "azurerm" {
     resource_group_name  = "vispoc-tfstate-rg"
     storage_account_name = "vispoctfstate3253"
