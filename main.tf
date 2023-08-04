a461218c504b47b8cf457c3c323359db796f3fc7
omarinux <81483478+omarinux@users.noreply.github.com>
2023-08-04 14:07:55 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,4 @@
-# Define Terraform provider
-#terraform {
-#  backend "azurerm" {
-#    resource_group_name  = "vispoc-tfstate-rg"
-#    storage_account_name = "vispoctfstate3253"
-#    container_name       = "tfstate"
-#    key                  = "actions.tfstate"
-#  }
 
-#  required_providers {
-#    azurerm = {
-#      version = "~>3.2"
-#      source  = "hashicorp/azurerm"
-#    }
-#  }
-#}
 # Configure the Azure provider
 provider "azurerm" { 
   features {}  
@@ -25,5 +10,6 @@ resource "azurerm_resource_group" "default" {
 
   tags = {
     environment = "test"
+    cost = "mycost"
   }
 }
