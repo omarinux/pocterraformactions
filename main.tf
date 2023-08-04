1d38e29cc49e23f6c8d5e12fda1fef6a1980c551
omarinux <81483478+omarinux@users.noreply.github.com>
2023-08-04 13:39:11 +0200

    Update and rename providers.tf to main.tf

--- main.tf
+++ main.tf
@@ -18,3 +18,12 @@ terraform {
 provider "azurerm" { 
   features {}  
 }
+
+resource "azurerm_resource_group" "default" {
+  name     = "firstrg20230804"
+  location = "East US 2"
+
+  tags = {
+    environment = "test"
+  }
+}
