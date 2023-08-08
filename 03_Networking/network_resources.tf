786985984359eb628ac517accb23ec190706934e
Omar Germana <omarinux@gmail.com>
2023-08-08 15:15:49 +0200

    net

--- 03_Networking/network_resources.tf
+++ 03_Networking/network_resources.tf
@@ -43,7 +43,8 @@ resource "azurerm_virtual_network" "test" {
 resource "local_file" "ansible_hosts_file" {
   content = templatefile("hosts.tpl", {
     virtualnetwork_name = azurerm_virtual_network.test.*.name,
-    addressspace_name   = azurerm_virtual_network.test.*.resource_group_name
+    addressspace_name   = azurerm_virtual_network.test.*.address_space
+    locationtpl = azurerm_virtual_network.test.*.location
   })
   filename = "hosts"
 }
